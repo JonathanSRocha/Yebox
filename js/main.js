cb1d380d55d58217c105666827c7c4f81654ff79
Jonathan Silva da Rocha <ART0060924@aluno.firjansenaisesi.com.br>
2023-08-31 12:16:06 -0300

    Subta

--- js/main.js
+++ js/main.js
@@ -10,7 +10,18 @@ let Start = setInterval(()=>{
     // Add your methods for Sanddox here
     YeEngine.AlterThemeSystem(YeEngine.Theme.SkyTheme)
     YeEngine.AlterTheme(YeEngine.Theme.GreyTheme)
-    
+ 
+    YeEngine.ModifyW(YeEngine.GetlementById("card"),"90vh")    
+    YeEngine.ModifyH(YeEngine.GetlementById("card"),"90vh")    
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
 },400) 
+
+let update = setInterval(() => {
+    if (YeEngine.Questions_save[0] != undefined)
+    {
+        YeEngine.ModifyContentClass(document.getElementById("card"),YeEngine.Questions_save[0])
+        clearInterval(update)
+  
+    }
+}, 600)
